527d30616054caa89f0eeb67625edebcf161f400
Leeroy Brun <leeroy.brun@gmail.com>
2015-10-28 16:47:30 +0100

    Initial commit

---
+++ HandsDetection/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace HandsDetection
+{
+    public partial class Form1 : Form
+    {
+        HandDetection detection = new HandDetection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> imgOrg;
+
+            try
+            {
+                imgOrg = new Image<Bgr, Byte>("D:\\dev\\HandsDetection\\HandsDetection\\img\\IMG_2358.JPG");
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+
+            if (imgOrg == null)
+            {
+                return;
+            }
+
+            List<Contour<Point>> contoursList = new List<Contour<Point>>();
+            List<Rectangle> boundingBoxes = new List<Rectangle>();
+
+            Image<Gray, Byte> imgProc = detection.FindSkinContours(imgOrg, contoursList, boundingBoxes);
+
+            Rectangle biggestBoundingBox = detection.FindBiggestBoundingBox(boundingBoxes);
+            Contour<Point> biggestContour = detection.FindBiggestContour(contoursList);
+
+            CvInvoke.cvDrawContours(imgOrg, biggestContour, new Bgr(Color.Transparent).MCvScalar, new Bgr(Color.Red).MCvScalar, 2, 0, Emgu.CV.CvEnum.LINE_TYPE.CV_AA, new Point(0, 0));
+
+            imgOrg.Draw(biggestBoundingBox, new Bgr(Color.Red), 2);
+
+            ImageBoxOrig.Image = imgOrg.Resize(0.5, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+            ImageBoxProc.Image = imgProc.Resize(0.5, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR);
+        }
+    }
+}
---
+++ HandsDetection/HandDetection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Windows.Forms;
+
+namespace HandsDetection
+{
+    public class HandDetection
+    {
+        // Cherche les contours et bounding boxes de la peau sur une image fournie
+        public Image<Gray, Byte> FindSkinContours(Image<Bgr, Byte> imgOrg, List<Contour<Point>> contoursList, List<Rectangle> boundingBoxes)
+        {
+            // Converti les couleurs de l'image en HSV
+            Image<Hsv, Byte> hsvImg = imgOrg.Convert<Hsv, Byte>();
+
+            // Filtre les pixels de l'image afin de ne garder que ceux qui se rapprochent de la couleur de la peau
+            Image<Gray, Byte> skin = hsvImg.InRange(new Hsv(0, 48, 80), new Hsv(20, 255, 255));
+
+            skin = skin.ThresholdBinary(new Gray(145), new Gray(255));
+
+            //skin = skin.SmoothGaussian(3);
+
+            // On erode et dilate pour éliminer les imperfections
+            skin = skin.Erode(7);
+            skin = skin.Dilate(7);
+
+            using (MemStorage storage = new MemStorage())
+            {
+                // Boucle sur tous les contours trouvés dans l'image
+                for (Contour<Point> contours = skin.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_TREE, storage); contours != null; contours = contours.HNext)
+                {
+                    // Si un contour est plus grand que 20x20 pixels, on le traite
+                    if (contours.BoundingRectangle.Width > 20 && contours.BoundingRectangle.Height > 20)
+                    {
+                        // On crée une bounding box autour du contour
+                        Rectangle rect = contours.BoundingRectangle;
+
+                        rect.X = rect.X - 10;
+                        rect.X = (rect.X < 0) ? 0 : rect.X;
+                        rect.Y = rect.Y - 10;
+                        rect.Y = (rect.Y < 0) ? 0 : rect.Y;
+
+                        rect.Height = (rect.Height + 20);
+                        rect.Width = (rect.Width + 20);
+
+                        // Ajout de la bounding box et du contour aux listes
+                        boundingBoxes.Add(rect);
+                        contoursList.Add(contours);
+                    }
+                }
+            }
+
+            return skin; // Retourne l'image avec uniquement la peau
+        }
+
+        // Trouve la plus grande bounding box
+        public Rectangle FindBiggestBoundingBox(List<Rectangle> boundingBoxes)
+        {
+            boundingBoxes.Sort((X, Y) => ((Y.Height * Y.Width).CompareTo(X.Height * X.Width)));
+
+            return boundingBoxes.First<Rectangle>();
+        }
+
+        // Trouve le plus grand contour
+        public Contour<Point> FindBiggestContour(List<Contour<Point>> contours)
+        {
+            contours.Sort((X, Y) => ((Y.BoundingRectangle.Height * Y.BoundingRectangle.Width).CompareTo(X.BoundingRectangle.Height * X.BoundingRectangle.Width)));
+
+            return contours.First<Contour<Point>>();
+        }
+    }
+}
